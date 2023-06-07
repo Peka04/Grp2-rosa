8bab23026d3bd4803d261bb099625083e7764e9c
saray1368 <rayat.sanaz.1368@gmail.com>
2023-06-07 12:42:24 +0000

    new filter & sort

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,6 +9,8 @@ sap.ui.define([
 
         return Controller.extend("shoprosarw.controller.Main", {
 
+
+
             onSelectionChangeProduct: function(oEvent){
                 var oItem = oEvent.getParameter("listItem");
                 var sPath = oItem.getBindingContext().getPath();
@@ -35,6 +37,32 @@ sap.ui.define([
                 } else if(oIcon == this.getView().byId("_IDGenIconTabFilter2")){
                     oSplitApp.toDetail("application-shoprosarw-display-component---Main--_IDGenPage3", "slide");
                 }
+            },
+            onPress: function(oEvent) {
+                var oInput = this.getView().byId("input00");
+                var sPrice = oInput.getValue();
+                var oInput1 = this.getView().byId("input01");
+                var sPrice1 = oInput1.getValue();
+                var oFilter = new sap.ui.model.Filter("Price", sap.ui.model.FilterOperator.BT, sPrice, sPrice1);
+                var oTable = this.getView().byId("idProductsTable");
+                oTable.getBinding("items").filter([oFilter]);
+                
+            },
+
+            onReset: function(oEvent) {
+                var oTable = this.getView().byId("idProductsTable");
+                oTable.getBinding("items").filter(null);
+			    oTable.getBinding("items").sort(null);
+			    var oInput = this.getView().byId("input00");
+			    oInput.setValue(null);
+                var oInput1 = this.getView().byId("input01");
+			    oInput1.setValue(null);
+            },
+
+            onSort: function(oEvent) {
+                var oSorter = new sap.ui.model.Sorter("Price", false);
+                var oTable = this.getView().byId("idProductsTable");
+                oTable.getBinding("items").sort([oSorter]);
             }
         });
     });
