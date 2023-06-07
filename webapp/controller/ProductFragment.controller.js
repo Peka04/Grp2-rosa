b5b4110527f279ee686001b9357d1b2b341f8693
saray1368 <rayat.sanaz.1368@gmail.com>
2023-06-07 12:21:40 +0000

    Productliste filter

--- webapp/controller/ProductFragment.controller.js
+++ webapp/controller/ProductFragment.controller.js
@@ -9,19 +9,51 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("shoprosarw.controller.ProductFragment", {
+
+            _oParent: null,
             onInit: function () { },
-            init: function(){
+
+            init: function(oParent){
+                this._oParent = oParent;
                 var oModel1 = new sap.ui.model.json.JSONModel;
-                this.getView().setModel(oModel1);
+                this._oParent.getView().setModel(oModel1);
+
+
+                 var oCore = sap.ui.getCore();
+                oCore.attachParseError(this.onFehler); 
             },
             
             onPress: function(oEvent) {
-                var oInput = this.getView().byId("input0");
+                var oInput = this.getView().byId("input00");
                 var sPrice = oInput.getValue();
-                var oFilter = new sap.ui.model.Filter("Price", sap.ui.model.FilterOperator.LT, sPrice);
-                var oTable = this.getView().byId("table01");
+                var oInput1 = this.getView().byId("input01");
+                var sPrice1 = oInput1.getValue();
+                var oFilter = new sap.ui.model.Filter("Price", sap.ui.model.FilterOperator.BT, sPrice, sPrice1);
+                var oTable = this.getView().byId("idProductsTable");
                 oTable.getBinding("items").filter([oFilter]);
                 
             },
+
+            onReset: function(oEvent) {
+                var oTable = this.getView().byId("idProductsTable");
+                oTable.getBinding("items").filter(null);
+			    oTable.getBinding("items").sort(null);
+			    var oInput = this.getView().byId("input00");
+			    oInput.setValue(null);
+                var oInput1 = this.getView().byId("input01");
+			    oInput1.setValue(null);
+            },
+
+            onSort: function(oEvent) {
+                var oSorter = new sap.ui.model.Sorter("Price", false);
+                var oTable = this.getView().byId("idProductsTable");
+                oTable.getBinding("items").sort([oSorter]);
+            },
+
+             onFehler: function(oEvent) {
+                var oElement = oEvent.getParameter("element");
+                oElement.setValueState(sap.ui.core.ValueState.Error);
+            } 
         });
     });
+
