f0e1badba6b1003ba72ede1a6bb5b4515790e783
kathrin-lehmann <kathrin.lehmann@adesso-orange.com>
2023-06-07 09:55:24 +0000

    fix navigation in icon tab bar

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -8,9 +8,6 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("shoprosarw.controller.Main", {
-            onInit: function () {
-
-            },
 
             onSelectionChangeProduct: function(oEvent){
                 var oItem = oEvent.getParameter("listItem");
@@ -21,7 +18,23 @@ sap.ui.define([
 
             onSelectionChangeCategory: function(oEvent){
                 //TODO: display all products for this category
-            }
+/*                 var oItem = oEvent.getParameter("listItem");
+                var sPath = oItem.getBindingContext().getPath();
+                var oTable = this.getView().byId("idCategoriesTable1");
+                oTable.bindElement( {
+                    path: sPath ,
+                    parameters: {expand: "Products"}
+                }); */
+            },
 
+            onIconTabSelect: function(oEvent){
+                var oSplitApp = this.getView().byId("_IDGenSplitApp1");
+                var oIcon = oEvent.getParameter("item");
+                if(oIcon == this.getView().byId("_IDGenIconTabFilter1")){
+                    oSplitApp.toDetail("application-shoprosarw-display-component---Main--_IDGenPage2", "slide");
+                } else if(oIcon == this.getView().byId("_IDGenIconTabFilter2")){
+                    oSplitApp.toDetail("application-shoprosarw-display-component---Main--_IDGenPage3", "slide");
+                }
+            }
         });
     });
