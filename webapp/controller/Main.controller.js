8ac8abc28cee94ced713fe0e6e94040835e75ea4
kathrin-lehmann <kathrin.lehmann@adesso-orange.com>
2023-06-08 13:43:49 +0000

    add fragment for product orders

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,33 +1,45 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("shoprosarw.controller.Main", {
 
-
-
             onSelectionChangeProduct: function(oEvent){
+                this.getView().byId("stepInput").setValue("0");
+                this.getView().byId("stepInput").setEnabled(true);
+
                 var oItem = oEvent.getParameter("listItem");
                 var sPath = oItem.getBindingContext().getPath();
                 var oTable = this.getView().byId("vlayoutk1");
                 oTable.bindElement(sPath);
             },
 
-            onSelectionChangeCategory: function(oEvent){
-                //TODO: display all products for this category
-/*                 var oItem = oEvent.getParameter("listItem");
+            onSelectionChangeCategory: function (oEvent) {
+                var oItem = oEvent.getParameter("listItem");
                 var sPath = oItem.getBindingContext().getPath();
                 var oTable = this.getView().byId("idCategoriesTable1");
-                oTable.bindElement( {
-                    path: sPath ,
-                    parameters: {expand: "Products"}
-                }); */
-            },
+                let oItemTemplate = new sap.m.ColumnListItem({
+                  cells: [
+                    new sap.m.Text({        
+                      text: "{Name}",      
+                    }),     
+                    new sap.m.Text({
+                      text: "{Price}",
+                    }),
+                   new sap.m.Text({
+                      text: "{Rating}",
+                    }),
+                  ],
+                });
+                let newPath = sPath + "/Products";
+                oTable.bindItems(newPath, oItemTemplate);
+              },
 
             onIconTabSelect: function(oEvent){
                 var oSplitApp = this.getView().byId("_IDGenSplitApp1");
@@ -63,6 +75,34 @@ sap.ui.define([
                 var oSorter = new sap.ui.model.Sorter("Price", false);
                 var oTable = this.getView().byId("idProductsTable");
                 oTable.getBinding("items").sort([oSorter]);
+            },
+
+             onAmountChange: function(oEvent){
+                var sValue = oEvent.getSource().getValue();
+                var sPrice = this.getView().byId("text9").getText();
+                this.wk = this.getOwnerComponent().getModel("orders");
+                let nOldAmount = this.wk.getProperty("/amount");
+                let nNewAmount = nOldAmount + Number(sPrice);
+                this.wk.setProperty("/amount", nNewAmount);
+                
+                let neueBestellung = {"productId": "", "name": this.getView().byId("text7").getText(), "menge": 1};
+                var json = this.wk.getProperty("/warenkorb");
+                json.push(neueBestellung);
+                this.wk.setProperty("/warenkorb", json);
+            } ,
+
+            sendOrder: function(oEvent){
+                MessageBox.show(
+                    "Bestellung erfolgreich.", {
+                        icon: MessageBox.Icon.INFORMATION,
+                        title: "Bestellung erfolgreich.",
+                        actions: [MessageBox.Action.YES],
+                    }
+                );
+                this.getView().byId("stepInput").setValue("0");
+                this.wk.setProperty("/amount", 0);
+                this.wk.setProperty("/warenkorb", new Array());
             }
+            
         });
     });
